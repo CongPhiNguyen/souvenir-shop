583fe2e204f9c613f6e6161dd2f70e968d52a9d4
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2021-10-14 20:28:52 +0700

    header

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,9 @@ const AuthController = require('../app/controllers/AuthController')
 
 router.get('/signin-with-password', AuthController.signinWithPassword);
 router.get('/signup-with-password', AuthController.signupWithPassword);
+router.get('/signin', (req, res) => {
+    res.render('signIn')
+})
 router.get('/', AuthController.renderHome)
 
 module.exports = router;
