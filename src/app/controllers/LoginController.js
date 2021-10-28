4adfb6b0956915073a51e8a839550cd4a15ab04c
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2021-10-28 11:20:06 +0700

    hello

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -5,57 +5,56 @@ const { deleteOne, exists } = require("./../models/user");
 
 class LoginController {
     // [GET] /
-    index(req, res){
-        res.render('login', {layout: 'layout2.hbs'});
+    index(req, res) {
+        res.render('login', { layout: 'layout2.hbs' });
     }
-    login(req, res){
-        let{username, password} = req.body;
+    login(req, res) {
+        let { username, password } = req.body;
         username.trim();
         password.trim();
 
-        if(username == "" || password == "" ){
+        if (username == "" || password == "" ) {
 
             req.session.message = {
                 type: 'warning-custom',
                 intro: 'Empty input fields!',
                 message: 'Please try again!'
-              }
-               res.redirect('login'); 
-        }else{
-            TaiKhoan.findOne({username: username}).then(data => {
-                if(data){
-                   const hashedPassword = data.password;
-                   bcrypt.compare(password, hashedPassword).then(result =>{
-                       if(result){
-                        res.redirect('home');
-                       }else{
-                        req.session.message = {
-                            type: 'danger-custom',
-                            intro: 'Incorrect password.',
-                            message: 'Please try again!'
-                          }
-                           res.redirect('login'); 
-                       }
-                   })
-                    
-                }else{
+            }
+            res.redirect('login');
+        } else {
+            TaiKhoan.findOne({ username: username }).then(data => {
+                if (data) {
+                    const hashedPassword = data.password;
+                    bcrypt.compare(password, hashedPassword).then(result => {
+                        if (result) {
+                            res.redirect('home');
+                        } else {
+                            req.session.message = {
+                                type: 'danger-custom',
+                                intro: 'Incorrect password.',
+                                message: 'Please try again!'
+                            }
+                            res.redirect('login');
+                        }
+                    })
 
+                } else {
                     req.session.message = {
                         type: 'danger-custom',
                         intro: 'User is not exists.',
                         message: 'Please try again!'
-                      }
-                       res.redirect('login'); 
-                   
+                    }
+                    res.redirect('login');
+
                 }
-            }).catch(err =>{
+            }).catch(err => {
                 console.log(err);
                 req.session.message = {
                     type: 'danger-custom',
                     intro: 'An error occcured while checking for existing user!',
                     message: 'Please try again!'
                 }
-                   res.redirect('login'); 
+                res.redirect('login');
             })
         }
 
--- src/app/controllers/SignUpController.js
+++ src/app/controllers/SignUpController.js
@@ -6,71 +6,70 @@ const user = require("./../models/user");
 class SignupController {
     // [GET] /
 
-    index(req, res){
-        res.render('signup', {layout: 'layout2.hbs'});
+    index(req, res) {
+        res.render('signup', { layout: 'layout2.hbs' });
     }
 
-    signup(req,res){
-        let {tenDangNhap, matKhau,xacNhanMatKhau, hoTen, soDienThoai, mail } = req.body;
+    signup(req, res) {
+        let { tenDangNhap, matKhau, xacNhanMatKhau, hoTen, soDienThoai, mail } = req.body;
         tenDangNhap = tenDangNhap.trim();
         matKhau = matKhau.trim();
         hoTen = hoTen.trim();
         soDienThoai = soDienThoai.trim();
         mail = mail.trim();
 
-        if(tenDangNhap == "" || mail == "" || matKhau == "" || hoTen == "" || soDienThoai == ""){
-
+        if (tenDangNhap == "" || mail == "" || matKhau == "" || hoTen == "" || soDienThoai == "") {
             req.session.message = {
                 type: 'warning-custom',
                 intro: 'Empty input fields!',
                 message: 'Please try again!'
-              }
-               res.redirect('signup'); 
-        }else if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(mail)){
-           
+            }
+            res.redirect('signup');
+        } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(mail)) {
+
             req.session.message = {
                 type: 'danger-custom',
                 intro: 'Invalid email entered!',
                 message: 'Please try again!'
-              }
-               res.redirect('signup'); 
-        }else if(matKhau.lenght < 8){
+            }
+            res.redirect('signup');
+        } else if (matKhau.lenght < 8) {
             req.session.message = {
                 type: 'warning-custom',
                 intro: 'Password is too short!',
                 message: 'Please try again!'
-              }
-               res.redirect('signup'); 
-        }else if(matKhau != xacNhanMatKhau){
-            
+            }
+            res.redirect('signup');
+        } else if (matKhau != xacNhanMatKhau) {
+
             req.session.message = {
                 type: 'danger-custom',
                 intro: 'Password is different with repeat password!',
                 message: 'Please try again!'
-              }
-               res.redirect('signup'); 
-        }else if(!/((09|03|07|08|05)+([0-9]{8})\b)/g.test(soDienThoai)){
+            }
+            res.redirect('signup');
+        } else if (!/((09|03|07|08|05)+([0-9]{8})\b)/g.test(soDienThoai)) {
 
             req.session.message = {
                 type: 'danger-custom',
                 intro: 'Invalid phone entered.',
                 message: 'Please try again!'
-              }
-               res.redirect('signup'); 
-        }else{
-            TaiKhoan.findOne({username: tenDangNhap}).then(result => {
-                if(result){
+            }
+            res.redirect('signup');
+        } else {
+            TaiKhoan.findOne({ username: tenDangNhap }).then(result => {
+                if (result) {
 
                     req.session.message = {
                         type: 'warning-custom',
                         intro: 'User already exists.',
                         message: 'Please try again!'
-                      }
-                       res.redirect('signup'); 
-                }else{
+                    }
+                    res.redirect('signup');
+                } else {
                     //create new user
                     const saltRound = 10;
-                    bcrypt.hash(matKhau, saltRound).then(hashedPassword =>{
+                    bcrypt.hash(matKhau, saltRound).then(hashedPassword => {
                         const newUser = new user({
                             username: tenDangNhap,
                             password: hashedPassword,
@@ -79,58 +78,56 @@ class SignupController {
                             mail: mail,
                             avatar: '',
                             address: '',
-                            role: 'user'
+                            role: 'user',
                         });
                         newUser.save().then(result => {
-                            if(result){
+                            if (result) {
                                 req.session.message = {
                                     type: 'success-custom',
                                     intro: '',
                                     message: 'Signup successful!'
-                                  }
-                                  res.redirect('signup'); 
+                                }
+                                res.redirect('signup');
                             }
                             else {
                                 req.session.message = {
                                     type: 'danger-custom',
                                     intro: 'FAILED!',
                                     message: 'Please try again'
-                                  }
-                                  res.redirect('signup'); 
+                                }
+                                res.redirect('signup');
                             }
                         })
+                            .catch(err => {
+                                req.session.message = {
+                                    type: 'danger-custom',
+                                    intro: 'An error occured while saving taiKhoan!',
+                                    message: 'Please try again!'
+                                }
+                                res.redirect('signup');
+                            })
+                    })
                         .catch(err => {
-
                             req.session.message = {
                                 type: 'danger-custom',
-                                intro: 'An error occured while saving taiKhoan!',
+                                intro: 'An error occured while hashing password!',
                                 message: 'Please try again!'
-                              }
-                               res.redirect('signup'); 
+                            }
+                            res.redirect('signup');
                         })
-                    })
-                    .catch(err => {
-
-                        req.session.message = {
-                            type: 'danger-custom',
-                            intro: 'An error occured while hashing password!',
-                            message: 'Please try again!'
-                          }
-                           res.redirect('signup'); 
-                    })
                 }
-            }).catch(err =>{
+            }).catch(err => {
                 console.log(err);
                 req.session.message = {
                     type: 'danger-custom',
                     intro: 'An error occcured while checking for existing user!',
                     message: 'Please try again!'
-                  }
-                   res.redirect('signup'); 
+                }
+                res.redirect('signup');
             })
         }
     }
-    
+
 }
 
 module.exports = new SignupController;
