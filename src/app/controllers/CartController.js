6db2689dc902d49e9db6ae76701dde4c1de39174
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2021-11-30 10:07:27 +0700

    cart

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -1,4 +1,6 @@
 const res = require("express/lib/response");
+const Cart = require('../models/cart');
+const Coupon = require("../models/coupon");
 
 class CartController {
     // [GET] /home
@@ -14,7 +16,7 @@ class CartController {
             res.render('cart/returnPolicy')
         }
         catch {
-            
+
         }
     }
 
@@ -33,13 +35,60 @@ class CartController {
 
     async deleteProductFromCart(req, res) {
         try {
-
             console.log(req.body.name)
             res.redirect('/cart')
         }
         catch {
 
         }
+    }
+
+    async addProductToCart(req, res) {
+        const { username, listCartabc } = req.body;
+        const newCart = await Cart.create({ username: username, listProduct: listCartabc })
+        newCart.save()
+            .then(result => {
+                res.status(201).json({ message: "Khởi tạo thành công" })
+            })
+            .catch(err => {
+                res.status(404).send(err);
+            })
+    }
+
+    async getCartUser(req, res) {
+        const { username } = req.body;
+        Cart.findOne({ username: username })
+            .then(result => {
+                if (result) {
+                    res.send(
+                        JSON.stringify({
+                            status: 201,
+                            cart: result,
+                        })
+                    );
+                }
+            })
+    }
+
+    async getCoupon(req, res) {
+        const { couponCode } = req.body
+        Coupon.findOne({couponCode}).then(result => {
+            if(result) {
+                res.send(
+                    JSON.stringify({
+                        status: 201,
+                        Coupon: result,
+                    })
+                )
+            } else {
+                res.status(403).send(
+                    JSON.stringify({
+                        status: 403,
+                        message: "Mã giảm giá không tồn tại",
+                    })
+                )
+            }
+        })
     }
 }
 
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,6 +5,9 @@ const CartController = require('../app/controllers/CartController');
 
 router.post('/raise-quantity', CartController.raiseQuantity);
 router.post('/delete-product-from-cart' , CartController.deleteProductFromCart)
+router.post('/add-product-to-cart', CartController.addProductToCart)
+router.post('/get-cart-user', CartController.getCartUser)
+router.post('/get-coupon', CartController.getCoupon)
 router.get('/return-policy', CartController.getReturnPolicy)
 router.get('/', CartController.getCart);
 
